ce813728c8c1686ad1a99e1072fe3b6b221c72a9
1codelife <rasheedtengo@gmail.com>
2025-04-07 16:02:54 -0500

    Update LuxuryCarShowroomApp.java

--- src/main/java/com/luxurycarshowroom/LuxuryCarShowroomApp.java
+++ src/main/java/com/luxurycarshowroom/LuxuryCarShowroomApp.java
@@ -1,1 +1,59 @@
+package com.luxurycarshowroom;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class LuxuryCarShowroomApp extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Luxury Car Showroom");
+
+        initRootLayout();
+        showCarOverview();
+    }
+
+    private void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(LuxuryCarShowroomApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showCarOverview() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(LuxuryCarShowroomApp.class.getResource("view/CarOverview.fxml"));
+            AnchorPane carOverview = loader.load();
+
+            rootLayout.setCenter(carOverview);
+
+            CarOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
